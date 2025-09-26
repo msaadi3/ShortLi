20a02a8812757109f856c8f0586bb392896c055a
Saad <113223551+msaadi3@users.noreply.github.com>
2025-09-26 23:57:43 +0500

    late commit

--- backend/src/db/connectDB.js
+++ backend/src/db/connectDB.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    mongoose.set('debug', true);
     const connectionResponse = await mongoose.connect(
       `${process.env.MONGODB_URI}/${process.env.DB}`
     );
